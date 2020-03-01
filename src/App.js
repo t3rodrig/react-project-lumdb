60bd993afd47cea217529011d9fbaae678b6cf43
Tonalli <tonalli@xanum.uam.mx>
2020-03-01 16:55:45 -0600

    Add Movie component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from "./Movie";
+
+const movies = [{
+    id: 1,
+    title: "Star Wars"
+  }, {
+    id: 2,
+    title: "Spider Man"
+  }, {
+    id: 3,
+    title: "36th Chamber of Shaolin"
+}];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      {movies.map(movie => (
+        <Movie key={movie.id} movie={movie}/>
+      ))}
     </div>
   );
 }

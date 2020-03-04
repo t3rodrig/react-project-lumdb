20297b7d5fdc9c20915b7f63da50df57a8f0740b
Tonalli <tonalli@xanum.uam.mx>
2020-03-04 12:43:49 -0600

    Add movie component

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default class Movie extends React.Component {
-    render() {
-        return (
-            <div>
-                {this.props.movie.title}
-            </div>
-        )
-    }
+  static propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired
+    }),
+    desc: PropTypes.string
+  }
+
+  static defaultProps = {
+    desc: "Description no available"
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>
+          {this.props.movie.title}
+        </h3>
+        <p>
+          {this.props.desc}
+        </p>
+      </div>
+    )
+  }
 }

c175463b376cee364c97a1980e50f7cba00ac81b
Tonalli <t3rodrig@users.noreply.github.com>
2021-02-02 17:58:14 -0600

    Refactor MovieDetail component
    use hooks

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Overdrive from 'react-overdrive';
 import { Poster } from './Movie';
@@ -8,48 +8,47 @@ const BACKDROP_PATH = `${process.env.REACT_APP_IMG_URL}/w1280`;
 const params = `api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
 const url = `${process.env.REACT_APP_API_URL}/movie`;
 
-class MovieDetail extends React.Component {
-  state = {
-    movie: {},
-  }
+const MovieDetail = (props) => {
+  const [movie, setMovie] = useState({});
 
-  async componentDidMount() {
+  const fetchMovie = async (arg) => {
+    const { id } = arg.match.params;
     try {
-      const res = await fetch(`${url}/${this.props.match.params.id}?${params}`);
-      const movie = await res.json();
-      this.setState({
-        movie,
-      });
+      const res = await fetch(`${url}/${id}?${params}`);
+      const data = await res.json();
+      setMovie(data);
     } catch (e) {
+      // eslint-disable-next-line no-console
       console.error(e);
     }
-  }
+  };
 
-  render() {
-    const { movie } = this.state;
-    return (
-      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
-        <MovieInfo>
-          <Overdrive id={movie.id}>
-            <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-          </Overdrive>
-          <div>
-            <h1>{movie.title}</h1>
-            <h3>{movie.release_date}</h3>
-            <p>{movie.overview}</p>
-          </div>
-        </MovieInfo>
-      </MovieWrapper>
-    );
-  }
-}
+  useEffect(() => {
+    fetchMovie(props);
+  }, []);
+
+  return (
+    <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+      <MovieInfo>
+        <Overdrive id={`${movie.id}`}>
+          <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+        </Overdrive>
+        <div>
+          <h1>{movie.title}</h1>
+          <h3>{movie.release_date}</h3>
+          <p>{movie.overview}</p>
+        </div>
+      </MovieInfo>
+    </MovieWrapper>
+  );
+};
 
 export default MovieDetail;
 
 const MovieWrapper = styled.div`
   position: relative;
   padding-top: 50vh;
-  background: url(${props => props.backdrop}) no-repeat;
+  background: url(${(props) => props.backdrop}) no-repeat;
   background-size: cover 
 `;
 

1e6eff4019098aad687a3d2fcd91315e5feca817
Tonalli <t3rodrig@users.noreply.github.com>
2021-02-02 16:39:32 -0600

    Fix Movie component
    Fix proTypes

--- src/Movie.js
+++ src/Movie.js
@@ -8,7 +8,7 @@ const POSTER_PATH = `${process.env.REACT_APP_IMG_URL}/w154`;
 
 const Movie = ({ movie }) => (
   <Link to={`/${movie.id}`}>
-    <Overdrive id={movie.id}>
+    <Overdrive id={`${movie.id}`}>
       <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
     </Overdrive>
   </Link>
@@ -18,7 +18,9 @@ export default Movie;
 
 Movie.propTypes = {
   movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
+    poster_path: PropTypes.string,
   }).isRequired,
 };
 
